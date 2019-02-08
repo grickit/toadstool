7f64a1e9f360bcfc808d7711e9df57f5f0174564
Derek Hoagland <thegrickit@gmail.com>
2019-02-07 22:56:54 -0500

    Forgot my footer was a wreck too.

--- private/views/footer.php
+++ private/views/footer.php
@@ -2,9 +2,9 @@
         </div>
         <footer class="footer">
             <div class="container">
-            <p style="margin-bottom: 4em;">High quality original photos available upon request and approval.</p>
-            <p>&copy; <?php echo date('Y'); ?> Derek Hoagland </p>
-            <p>Powered by <a href="https://github.com/grickit/toadstool">Toadstool</a></p>
+                <p style="margin-bottom: 4em;">High quality original photos available upon request and approval.</p>
+                <p>&copy; <?php echo date('Y'); ?> Derek Hoagland </p>
+                <p>Powered by <a href="https://github.com/grickit/toadstool">Toadstool</a></p>
             </div>
         </footer>
     </body>
